90023853f7728fe0c7d5da085eddb761adf5705d
Robson Rodrigues <147940582+RRDMELO@users.noreply.github.com>
2025-02-17 17:49:20 -0300

    feat Repetindo Textos

---
+++ Repetindo Textos/main.py
@@ -0,0 +1,9 @@
+# Solicitar uma string e um número inteiro como entrada
+string = input("Digite uma string: ")
+num = int(input("Digite um número inteiro: "))
+
+# Repetir a string o número de vezes informado
+resultado = string * num
+
+# Exibir o resultado
+print("O resultado da repetição é:", resultado)
