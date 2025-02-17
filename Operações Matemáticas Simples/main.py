e97e7dcc105753ae2210f17e2f111b833697fc05
Robson Rodrigues <147940582+RRDMELO@users.noreply.github.com>
2025-02-17 17:50:38 -0300

    feat Operações Matemáticas Simples

---
+++ Operações Matemáticas Simples/main.py
@@ -0,0 +1,9 @@
+# Solicitar dois números como entrada
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+# Realizar uma operação simples (soma)
+resultado = num1 + num2
+
+# Exibir o resultado
+print("O resultado da soma é:", resultado)
