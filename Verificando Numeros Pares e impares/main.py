b92af8c1e33de8f522afdd36eade33e8de2e89b9
Robson Rodrigues <147940582+RRDMELO@users.noreply.github.com>
2025-02-17 17:51:46 -0300

    feat Verificando Numeros Pares e impares

---
+++ Verificando Numeros Pares e impares/main.py
@@ -0,0 +1,8 @@
+# Solicitar um número inteiro como entrada
+num = int(input("Digite um número inteiro: "))
+
+# Verificar se o número é par ou ímpar
+if num % 2 == 0:
+    print("O número é par.")
+else:
+    print("O número é ímpar.")
