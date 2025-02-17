285d2a8ca1673db782750dff5ad2d4edc6aff775
Robson Rodrigues <147940582+RRDMELO@users.noreply.github.com>
2025-02-17 17:44:47 -0300

    feat concatenação em python

---
+++ projeto-python/main.py
@@ -0,0 +1,9 @@
+# Primeiro, vamos pedir ao usuário para inserir dois dados diferentes
+dado1 = input("Insira o primeiro dado: ")
+dado2 = input("Insira o segundo dado: ")
+
+# Concatenando os dados
+resultado = dado1 + dado2
+
+# Exibindo o resultado
+print("O resultado da concatenação é:", resultado)
